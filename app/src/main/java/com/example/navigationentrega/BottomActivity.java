d0f0d25e2b7e6ef0393816b2ce33e37091bf9c62
aitorsp-elpuig <aitorsp@elpuig.xeill.net>
2020-10-14 23:02:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/navigationentrega/BottomActivity.java
@@ -0,0 +1,26 @@
+package com.example.navigationentrega;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import com.example.navigationentrega.databinding.ActivityBottomBinding;
+
+public class BottomActivity extends AppCompatActivity {
+
+    ActivityBottomBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((binding = ActivityBottomBinding.inflate(getLayoutInflater())).getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+        NavigationUI.setupWithNavController(binding.toolbar, navController);
+    }
+}
